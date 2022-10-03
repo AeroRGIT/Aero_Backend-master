64265e9f7561cc542b164d7b948f2b5da29f0371
AeroRGIT <aerorgit@gmail.com>
2022-10-03 19:05:20 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -140,5 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ORIGIN_WHITELIST = (
-    'https://aerorgit.co.in',
+    'https://www.aerorgit.co.in/',
 )
